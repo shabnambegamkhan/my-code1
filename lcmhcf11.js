b25c80ff59e7392b4e39b4b9b443e9078de8e32f
shabnambegamkhan <163388394+shabnambegamkhan@users.noreply.github.com>
2024-05-23 20:16:21 +0530

    Add files via upload

---
+++ lcmhcf11.js
@@ -0,0 +1,18 @@
+
+
+// const input = require("readline-sync")
+// let a = input.question("enter the number")
+// let b = input.question("enter the number")
+
+// let i=0;
+// while(i<=a && i<=b){
+//     if(a%i==0  &&  b%i==0){
+//        hcf=i;
+//     }
+//       i++;
+// }
+    
+// lcm=a*b/hcf
+// console.log(hcf);
+// console.log(lcm);
+
